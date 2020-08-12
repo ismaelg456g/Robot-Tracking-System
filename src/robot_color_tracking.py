dc8d6846446038851bb9dd39fe6c7249cb277f9e
ismaelg456g <ismael-456@hotmail.com>
2020-08-12 17:56:29 -0300

    print multiple colors

--- src/robot_color_tracking.py
+++ src/robot_color_tracking.py
@@ -7,21 +7,23 @@ import cv2
 from scipy.ndimage import measurements, morphology
 
 class Colors(Enum):
-	red = 180
-	orange = 15
-	yellow = 30
-	chartreuse = 45
+	red = 0
 	green = 60
-	springGreen = 75
-	cyan = 90
-	skyBlue = 105
 	blue = 120
-	violet = 135
+
+	yellow = 30
+	cyan = 90
 	magenta = 150
+
+	orange = 15
+	chartreuse = 45
+	springGreen = 75
+	skyBlue = 105
+	violet = 135
 	pink = 165
 
 class RobotColorTracking(object):
-	def __init__(self, binaryThreshold = 110, hueTolerance = 7, satTolerance = 60, kernel=ones((20,20))):
+	def __init__(self,nbr_colors = 3, binaryThreshold = 110, hueTolerance = 7, satTolerance = 60, kernel=ones((20,20))):
 		self.binaryThreshold= binaryThreshold
 		self.hueTolerance = hueTolerance
 		self.satTolerance = satTolerance
@@ -30,7 +32,15 @@ class RobotColorTracking(object):
 		self.labels = None
 		self.image = None
 		self.nbr_objects = None
-		self.pose = None
+		self.pose = {}
+
+		self.colors = []
+		i = 0
+		for color in Colors:
+			self.colors.append(color)
+			i+=1
+			if i >= nbr_colors:
+				break
 
 	def _segmentColor(self,image, color):
 		image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -54,16 +64,17 @@ class RobotColorTracking(object):
 	def _trackByColor(self,image, color):
 		image = self._segmentColor(image, color)
 		image = self._filterImage(image)
-		self.image = image
 		self.labels, self.nbr_objects = measurements.label(image)
 		center_of_mass = array(measurements.center_of_mass(image, labels=self.labels, index=range(1,self.nbr_objects+1) ), dtype=float)
 
 		return center_of_mass
 
-	def track(self,image_name, colors): #lista de cores
+	def track(self,image_name): #lista de cores
 		image = cv2.imread(image_name)
-		#for percorrendo cores
-		self.pose = self._trackByColor(image, Colors.orange.value) #all colors
+		self.image = image
+		
+		for color in self.colors:
+			self.pose[color.name] = self._trackByColor(image, color.value)
 
 
 	def printRobotLocation(self): #precisa dos labels e nbr_objects
@@ -74,9 +85,9 @@ class RobotColorTracking(object):
 		figure(figsize=(50,50))
 		gray()
 		imshow(self.image)
-
-		for i in range(self.nbr_objects):
-		    text(self.pose[i][1], self.pose[i][0], 'Objeto', color='black', horizontalalignment='center',verticalalignment='center')
+		for color in self.colors:
+			for i in range(self.nbr_objects):
+			    text(self.pose[color.name][i][1], self.pose[color.name][i][0], color.name, color='red', horizontalalignment='center',verticalalignment='center')
 
 
 
