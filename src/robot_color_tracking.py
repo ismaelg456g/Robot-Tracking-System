54d0ff84452f9f9803014091f954c1f5dca6ee8e
ismaelg456g <ismael-456@hotmail.com>
2020-09-21 16:28:41 -0300

    testing geometric detect

--- src/robot_color_tracking.py
+++ src/robot_color_tracking.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from enum import Enum
 from PIL import Image
 import cv2
+import imutils
 from scipy.ndimage import measurements, morphology
 
 #https://www.pyimagesearch.com/2016/02/08/opencv-shape-detection/
@@ -190,3 +191,69 @@ class HoughColorTrack(RobotTracking):
 			imshow(self._segmentedImages[self._colors[nbr].name])
 		else:
 			print('Use debug=True for utilizing this method')
+
+
+class GeometricTrack(RobotTracking):
+	def track(self, image_name):
+		image = cv2.imread(image_name)
+		resized = imutils.resize(image, width=300)
+		ratio = image.shape[0] / float(resized.shape[0])
+		gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+		blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+		thresh = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY)[1]
+	
+		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+		cnts = imutils.grab_contours(cnts)
+		
+		for c in cnts:
+			# compute the center of the contour, then detect the name of the
+			# shape using only the contour
+			M = cv2.moments(c)
+			cX = int((M["m10"] / M["m00"]) * ratio)
+			cY = int((M["m01"] / M["m00"]) * ratio)
+			shape = self._detect(c)
+			# multiply the contour (x, y)-coordinates by the resize ratio,
+			# then draw the contours and the name of the shape on the image
+			c = c.astype("float")
+			c *= ratio
+			c = c.astype("int")
+			cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+			cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, (255, 255, 255), 2)
+			# show the output image
+			cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+			cv2.resizeWindow('image', 600,600)
+			cv2.imshow("image", image)
+			cv2.waitKey(0)
+
+		return thresh
+	def _detect(self, c):
+		# initialize the shape name and approximate the contour
+		shape = "unidentified"
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+		# if the shape is a triangle, it will have 3 vertices
+		if len(approx) == 3:
+			shape = "triangle"
+		# if the shape has 4 vertices, it is either a square or
+		# a rectangle
+		elif len(approx) == 4:
+			# compute the bounding box of the contour and use the
+			# bounding box to compute the aspect ratio
+			(x, y, w, h) = cv2.boundingRect(approx)
+			ar = w / float(h)
+			# a square will have an aspect ratio that is approximately
+			# equal to one, otherwise, the shape is a rectangle
+			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+		# if the shape is a pentagon, it will have 5 vertices
+		elif len(approx) == 5:
+			shape = "pentagon"
+		# otherwise, we assume the shape is a circle
+		else:
+			shape = "circle"
+		# return the name of the shape
+		return shape
+
+
+
